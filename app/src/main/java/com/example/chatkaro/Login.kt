4a27e32c58c3f7279df2cfa14b046cd602a4f5d5
Autri <autrininja@gmail.com>
2022-07-10 15:24:22 +0530

    second

---
+++ app/src/main/java/com/example/chatkaro/Login.kt
@@ -0,0 +1,66 @@
+package com.example.chatkaro
+
+import android.content.ContentValues
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_login.*
+
+
+class Login : AppCompatActivity() {
+        private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        supportActionBar?.hide()
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+        btn_signup.setOnClickListener {
+            startActivity(Intent(this,SignUp::class.java))
+        }
+
+        btn_login.setOnClickListener {
+            val email = edt_email.text.toString()
+            val password = edt_password.text.toString()
+
+            if (email.isNotEmpty()||password.isNotEmpty())
+                login(email,password)
+            else
+                Toast.makeText(this,"Please Fill in the details", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun login(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    val intent=Intent(this,MainActivity::class.java)
+                    finish()
+                    startActivity(intent)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(ContentValues.TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            startActivity(intent);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/chatkaro/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.chatkaro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var userRecyclerView: RecyclerView
+    private lateinit var userList: ArrayList<User>
+    private lateinit var adapter: UserAdapter
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.show()
+
+        auth = Firebase.auth
+
+        userList= ArrayList()
+        adapter=UserAdapter(this,userList)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+}
